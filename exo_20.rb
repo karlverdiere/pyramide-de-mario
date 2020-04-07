6c05b0f12adc4f93d762f7581b10a8b63e7da8b0
karlverdiere <karl.verdiere@hotmail.fr>
2020-04-07 21:58:06 +0200

    ma pyramide

--- exo_20.rb
+++ exo_20.rb
@@ -2,24 +2,14 @@ def pyramide
 puts"combien d'étage veut tu"
 number = gets.chomp.to_i
 i = 1
-e = 1
 j = number
 puts "voici donc cette fameuse pyramide:"
 	while j >= 1
-		j.times {print " "}
+
 		i.times {print "#"}
 
 		puts "\n"
 		i = i + 1
 		j = j - 1
 	end
-end
-pyramide
-
-n = 4
-br = "\n" * 2
-
-puts "Right triangle:#{br}"
-for i in 1..n do
-  puts "* " * i
 end
